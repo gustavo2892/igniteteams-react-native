3cfd7eac67c2df5fc2ce9de21950e96ac2bc75dc
Gustavo Freitas <gustavo.freitas@venturus.org.br>
2022-09-21 15:41:32 -0300

    Theme

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,11 @@
-import { Container, Title } from './styles';
+import * as S from './styles';
 
 export function Groups() {
   return (
-    <Container>
-      <Title>
+    <S.Container>
+      <S.Title>
         Groups
-      </Title>
-    </Container>
+      </S.Title>
+    </S.Container>
   );
 };
