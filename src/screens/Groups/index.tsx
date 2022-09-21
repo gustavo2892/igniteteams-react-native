7042d7968ca9e1e26269fc8239546b8ee28d55ae
Gustavo Freitas <gustavo.freitas@venturus.org.br>
2022-09-21 15:51:51 -0300

    Add types theme

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,7 @@
-import * as S from './styles';
+import { Container } from './styles';
 
 export function Groups() {
   return (
-    <S.Container>
-      <S.Title>
-        Groups
-      </S.Title>
-    </S.Container>
+    <Container/>
   );
-};
+}
