7d0bb7cfab70ed49b700e6f5f842eeba1638e481
Gustavo Freitas <gustavo.freitas@venturus.org.br>
2022-09-26 13:48:57 -0300

    Font roboto

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
-import { ThemeProvider } from 'styled-components';
+import { ActivityIndicator } from 'react-native';
+import { ThemeProvider } from 'styled-components/native'; 
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
+import theme from './src/theme';
 import { Groups } from '@screens/Groups';
-import theme from '@theme/index';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      { fontsLoaded ? <Groups/> : <ActivityIndicator /> }
     </ThemeProvider>
   );
 };
