abd5252822c5b347057bad8a530404a030aab435
Gustavo Freitas <gustavo.freitas@venturus.org.br>
2022-09-27 15:02:15 -0300

    Group state

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,14 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { Container } from './styles';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galera do Ignite']);
+
   return (
     <Container>
       <Header />
@@ -11,7 +16,15 @@ export function Groups() {
         title="Turmas"
         subtitle="jogue com a sua turma"
       />
-      <GroupCard title="Galera do Ignite" />
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard 
+            title={item} 
+          />
+        )}
+      />
     </Container>
   );
 };
