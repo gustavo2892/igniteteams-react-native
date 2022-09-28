dbcbe5bf97e4d0800ef7e66f3632675838d6bd4c
Gustavo Freitas <gustavo.freitas@venturus.org.br>
2022-09-28 09:02:45 -0300

    List Empty component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,6 +4,7 @@ import { FlatList } from 'react-native';
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { ListEmpty } from '@components/ListEmpty';
 import { Container } from './styles';
 
 export function Groups() {
@@ -23,6 +24,10 @@ export function Groups() {
           <GroupCard 
             title={item} 
           />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
       />
     </Container>
