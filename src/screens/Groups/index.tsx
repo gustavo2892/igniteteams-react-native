248b0fa51cdecad3cbfba65fe2f559290a72d35c
Gustavo Freitas <gustavo.freitas@venturus.org.br>
2022-09-28 14:12:17 -0300

    Button Component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { FlatList } from 'react-native';
 
+import { Button } from '@components/Button';
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
@@ -29,6 +30,9 @@ export function Groups() {
         ListEmptyComponent={() => (
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
+      />
+      <Button 
+        title='Criar nova turma'
       />
     </Container>
   );
