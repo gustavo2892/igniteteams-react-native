e0468e32933be5ca8cc9f4f49ce3a2d8cf910650
Gustavo Freitas <gustavo.freitas@venturus.org.br>
2022-09-30 10:08:49 -0300

    Screen New Group

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 
 import theme from './src/theme';
 import { Groups } from '@screens/Groups';
+import { NewGroup } from '@screens/NewGroup';
 import { Loading } from '@components/Loading';
 
 export default function App() {
@@ -16,7 +17,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <Groups/> : <Loading /> }
+      { fontsLoaded ? <NewGroup/> : <Loading /> }
     </ThemeProvider>
   );
 };
