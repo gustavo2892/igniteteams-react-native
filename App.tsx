805dfd40a1fc1a8d899910b13766d8deaa733e18
Gustavo Freitas <gustavo.freitas@venturus.org.br>
2022-09-30 11:36:19 -0300

    Players screens

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 import theme from './src/theme';
 import { Groups } from '@screens/Groups';
 import { NewGroup } from '@screens/NewGroup';
+import { Players } from '@screens/Players';
 import { Loading } from '@components/Loading';
 
 export default function App() {
@@ -17,7 +18,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <Groups/> : <Loading /> }
+      { fontsLoaded ? <Players/> : <Loading /> }
     </ThemeProvider>
   );
 };
